0d5cc7facf242ed6a5c5064d09974a1f4962cd4b
Naji4054 <mhdnaji4054@gmail.com>
2025-07-20 11:00:12 +0530

    home page working on js objectmap

--- home.js
+++ home.js
@@ -117,19 +117,22 @@ const instaPost = [
 
 ]
 
-const instagram = document.getElementById("instagram");
-let postList = ''
+const createInstaPost = (insta, instaContainer)=>{
+    const instagram = document.getElementById(instaContainer);
+    let postList = ''
 
-instaPost.forEach((insta)=>{
-postList += ` <div class="insta-wrap">
-                        <img src=${insta.image} alt="insta1">
-                    </div>`
-})
+    insta.forEach((instaPic)=>{
+    postList += ` <div class="insta-wrap">
+                            <img src=${instaPic.image} alt="insta1">
+                        </div>`
+    })
+    console.log(postList);
+    instagram.innerHTML = postList;
+    instagram.classList.add("contianer");
+}
 
+createInstaPost(instaPost, 'instagram');
 
-console.log(postList);
-instagram.innerHTML = postList;
-instagram.classList.add("contianer");
 
 
 // Home Banner
@@ -154,54 +157,69 @@ const bannerSection = [
     
 ]
 
-const banner = document.getElementById("banner-container");
-let bannerList = '';
+const createBanner = (banner1 , bannerContainer) => {
+    const banner = document.getElementById(bannerContainer);
+    let bannerList = '';
 
-bannerSection.forEach((homeBanner)=>{
-    bannerList += `<div class ="banner-wrap">
-                        <img src=${homeBanner.image} alt="banner1">
-                        <div  class="des-box">
-                            <div class="banner-p">
-                                <P>${homeBanner.deal}<P>
-                            </div>
-                            <div class="banner-h4">
-                               <h4>${homeBanner.h4}<h4>
-                            </div>
-                            <div class="button-wrap">
-                                <button class="banner-button">${homeBanner.button}</button>
-                            </div>
-                        </div>
-
-
-                    </div>`
-});
-
-
-bannerList += `<div class ="banner-wrap">
-                        <img src="./assets/banner3.jpg" alt="banner3">
-                        <div class="banner-3">
+    banner1.forEach((homeBanner)=>{
+        bannerList += `<div class ="banner-wrap">
+                            <img src=${homeBanner.image} alt="banner1">
                             <div  class="des-box">
                                 <div class="banner-p">
-                                    <P>SUMMER</P>
+                                    <P>${homeBanner.deal}<P>
                                 </div>
                                 <div class="banner-h4">
-                                    <h4>CLEARENCE</h4>
-                                </div>
-                                <div class="sale-banner">
-                                    <img src="./assets/50percent.png" alt="sale50">
-                                </div>
-                                <div class="banner-p2">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde in corrupti veniam beatae perspiciatis aperiam nobis, nemo quam blanditiis minus.</p>
+                                <h4>${homeBanner.h4}<h4>
                                 </div>
                                 <div class="button-wrap">
-                                    <button class="banner-button">DISCOVER NOW</button>
+                                    <button class="banner-button">${homeBanner.button}</button>
                                 </div>
                             </div>
 
-                        </div>
 
-                    </div>`
+                        </div>`
+    });
 
-banner.innerHTML = bannerList;
 
-console.log(banner)
+    bannerList += `<div class ="banner-wrap">
+                            <img src="./assets/banner3.jpg" alt="banner3">
+                            <div class="banner-3">
+                                <div  class="des-box">
+                                    <div class="banner-p">
+                                        <P>SUMMER</P>
+                                    </div>
+                                    <div class="banner-h4">
+                                        <h4>CLEARENCE</h4>
+                                    </div>
+                                    <div class="sale-banner">
+                                        <img src="./assets/50percent.png" alt="sale50">
+                                    </div>
+                                    <div class="banner-p2">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde in corrupti veniam beatae perspiciatis aperiam nobis, nemo quam blanditiis minus.</p>
+                                    </div>
+                                    <div class="button-wrap">
+                                        <button class="banner-button">DISCOVER NOW</button>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>`
+
+    banner.innerHTML = bannerList;
+
+    console.log(banner)
+
+}
+
+createBanner(bannerSection, 'banner-container');
+
+
+
+    let dataArr;
+
+ fetch('https://687a124fabb83744b7eb4962.mockapi.io/api/learn/commerce-prodscts').then(res=> res.json()).then(data=> {
+    dataArr = data
+})
+
+console.log(dataArr)
